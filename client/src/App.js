34a0ab678647313fc9071e3d0512ce4d21ede88a
lee2ton <lee@2ton.com>
2022-02-01 01:52:40 -0500

    Initial project setup with full-stack environment

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>QuickCh@ app</h1>
+      <p>Page intentionally left blank.</p>
+    </>
+  );
+};
+
+export default App;
