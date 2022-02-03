e17ea3eab56484cacbe6f23d8a1cd12a11c8f72c
Lee <leedotpang@gmail.com>
2022-02-03 01:23:35 -0500

    Fixed cors on server for api requests from app

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import connectToDB from "./database/index.js";
 import { messageRoute, roomRoute, userRoute } from "./routes/index.js";
 
@@ -7,6 +8,7 @@ const app = express();
 connectToDB();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (_, res) =>
   res.json({ message: "Route intentionally left blank." })
